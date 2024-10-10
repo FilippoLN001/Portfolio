2ec3e86f35e1e8a7007bd38b78dca61555460768
FilippoLN001 <filippo.niculae@gmail.com>
2024-10-10 13:12:52 +0200

    first commit

---
+++ dillionverma-portfolio-bb8dd0a/src/data/resume.tsx
@@ -0,0 +1,526 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Filippo Leonardo Niculae",
+  initials: "FLN",
+  url: "https://www.linkedin.com/in/filippo-leonardo-niculae-25b470279/",
+  location: "Terni TR; ITALY",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    " Full stack Junior Developer. I love building things",
+  summary:
+    "I am a highly motivated and flexible professional with a strong academic background obtained from the Department of Mathematics and Computer Science at the University of Perugia. I stand out for my adaptability to new situations and work environments, the ability to work effectively in teams, and my determination to pursue new skills in the field of computer science.",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js (learning...)",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Postgres",
+    "laravel",
+    "Angular",
+    "Java",
+    "C/C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "filippo.niculae@gmail.com",
+    tel: "+39 3295669771",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/FilippoLN001",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/filippo-leonardo-niculae-25b470279/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "mailto:filippo.niculae@gmail.com?",
+        icon: Icons.email,
+
+        navbar: true,
+      },
+    },
+  },
+
+  work: [],
+  education: [
+    {
+      school: "University of Perugia",
+      href: "https://www.unipg.it/",
+      degree: "Bachelor's Degree of Information Tecnology (IT)",
+      logoUrl: "/unipg_logo.png",
+      start: "2020",
+      end: "2024",
+    },
+    {
+      school: "International Baccalaureate",
+      href: "https://www.ittterni.edu.it/",
+      degree: "IT Diploma",
+      logoUrl: "",
+      start: "2015",
+      end: "2020",
+    },
+  ],
+  projects: [
+    {
+      title: "Real time Chat",
+      href: "",
+      dates: "Oct 2024 - Current",
+      active: true,
+      description:
+        "Developed an open-source Realtime chat with React.",
+      technologies: [
+        "React",
+        "Soket.io",
+        "UIverse",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "./realtimechat.mp4",
+    },
+    {
+      title: "Tesis Project 'TadPole'",
+      href: "https://github.com/FilippoLN001/Tesi-FPGA",
+      dates: "Jun 2024 - Jul 2024",
+      active: true,
+      description:
+        " Development of a system for monitoring and analyzing triggers and busy logic for data analysis systems in astroparticle physics",
+      technologies: [
+        "Next.js",
+        "Typescript",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/FilippoLN001/Tesi-FPGA",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/tadpole.png",
+      video:
+        "",
+    },
+    {
+      title: "TechHive",
+      href: "https://github.com/FilippoLN001/esame_finale_programmazione",
+      dates: "Jun 2024 - Jun 2024",
+      active: true,
+      description:
+        "Tech Ecommerce with admin manage.",
+      technologies: [
+        "Node.js",
+        "Typescript",
+        "MySQL",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/FilippoLN001/esame_finale_programmazione",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/techHive.png",
+      video: "",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Telegram bot",
+      dates: "December 18rd -  Jenuary 25th, 2023",
+      location: "Perugia , PG",
+      description:
+        "Telegram bot so simplify lessons and exam research",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg",
+      mlh: "",
+      links: [
+        {
+          title: "GitHub",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/FilippoLN001/bot_telegram",
+        },
+      ],
+    },
+    {
+      title: "IKEA Room with Blender and Unity",
+      dates: "March 14th - April 8th, 2024",
+      location: "Perugia PG",
+      description:
+        "Developed a 3D room with modellation software 'blender' and get explorable with 'unity'.",
+      image:
+        "/blender_unity.png",
+      mlh: "",
+      links: [],
+    },
+    {
+      title: "DeveloperWeek Hackathon",
+      dates: "February 3rd - 4th, 2018",
+      location: "San Francisco, California",
+      description:
+        "Developed a web application which aggregates social media data regarding cryptocurrencies and predicts future prices.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/developer-week.jpg",
+      links: [
+        {
+          title: "Github",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/cryptotrends/cryptotrends",
+        },
+      ],
+    },
+    {
+      title: "HackDavis",
+      dates: "January 20th - 21st, 2018",
+      location: "Davis, California",
+      description:
+        "Developed a mobile application which allocates a daily carbon emission allowance to users to move towards a sustainable environment.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-davis.png",
+      win: "Best Data Hack",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2018/white.svg",
+      links: [
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/my6footprint",
+        },
+        {
+          title: "ML",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/my6footprint-machine-learning",
+        },
+        {
+          title: "iOS",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/CarbonWallet",
+        },
+        {
+          title: "Server",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/wallet6-server",
+        },
+      ],
+    },
+    {
+      title: "ETH Waterloo",
+      dates: "October 13th - 15th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed a blockchain application for doctors and pharmacists to perform trustless transactions and prevent overdosage in patients.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/eth-waterloo.png",
+      links: [
+        {
+          title: "Organization",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/ethdocnet",
+        },
+      ],
+    },
+    {
+      title: "Hack The North",
+      dates: "September 15th - 17th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed a virtual reality application allowing users to see themselves in third person.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-north.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Streamer Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/justinmichaud/htn2017",
+        },
+        {
+          title: "Client Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/RTSPClient",
+        },
+      ],
+    },
+    {
+      title: "Hack The 6ix",
+      dates: "August 26th - 27th, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed an open platform for people shipping items to same place to combine shipping costs and save money.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-6ix.jpg",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/ShareShip/ShareShip",
+        },
+        {
+          title: "Site",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://share-ship.herokuapp.com/",
+        },
+      ],
+    },
+    {
+      title: "Stupid Hack Toronto",
+      dates: "July 23rd, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed a chrome extension which tracks which facebook profiles you have visited and immediately texts your girlfriend if you visited another girls page.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/stupid-hackathon.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/nsagirlfriend/nsagirlfriend",
+        },
+      ],
+    },
+    {
+      title: "Global AI Hackathon - Toronto",
+      dates: "June 23rd - 25th, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed a python library which can be imported to any python game and change difficulty of the game based on real time emotion of player. Uses OpenCV and webcam for facial recognition, and a custom Machine Learning Model trained on a [Kaggle Emotion Dataset](https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge/leaderboard) using [Tensorflow](https://www.tensorflow.org/Tensorflow) and [Keras](https://keras.io/). This project recieved 1st place prize at the Global AI Hackathon - Toronto and was also invited to demo at [NextAI Canada](https://www.nextcanada.com/next-ai).",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/global-ai-hackathon.jpg",
+      win: "1st Place Winner",
+      links: [
+        {
+          title: "Article",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://syncedreview.com/2017/06/26/global-ai-hackathon-in-toronto/",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/TinySamosas/",
+        },
+      ],
+    },
+    {
+      title: "McGill AI for Social Innovation Hackathon",
+      dates: "June 17th - 18th, 2017",
+      location: "Montreal, Quebec",
+      description:
+        "Developed realtime facial microexpression analyzer using AI",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/ai-for-social-good.jpg",
+      links: [],
+    },
+    {
+      title: "Open Source Circular Economy Days Hackathon",
+      dates: "June 10th, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed a custom admin interface for food waste startup <a href='http://genecis.co/'>Genecis</a> to manage their data and provide analytics.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/open-source-circular-economy-days.jpg",
+      win: "1st Place Winner",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/genecis",
+        },
+      ],
+    },
+    {
+      title: "Make School's Student App Competition 2017",
+      dates: "May 19th - 21st, 2017",
+      location: "International",
+      description: "Improved PocketDoc and submitted to online competition",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/make-school-hackathon.png",
+      win: "Top 10 Finalist | Honourable Mention",
+      links: [
+        {
+          title: "Medium Article",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://medium.com/make-school/the-winners-of-make-schools-student-app-competition-2017-a6b0e72f190a",
+        },
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+    {
+      title: "HackMining",
+      dates: "May 12th - 14th, 2017",
+      location: "Toronto, Ontario",
+      description: "Developed neural network to optimize a mining process",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-mining.png",
+      links: [],
+    },
+    {
+      title: "Waterloo Equithon",
+      dates: "May 5th - 7th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed Pocketdoc, an app in which you take a picture of a physical wound, and the app returns common solutions or cures to the injuries or diseases.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/waterloo-equithon.png",
+      links: [
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+    {
+      title: "SpaceApps Waterloo",
+      dates: "April 28th - 30th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed Earthwatch, a web application which allows users in a plane to virtually see important points of interest about the world below them. They can even choose to fly away from their route and then fly back if they choose. Special thanks to CesiumJS for providing open source world and plane models.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/space-apps.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/earthwatch",
+        },
+      ],
+    },
+    {
+      title: "MHacks 9",
+      dates: "March 24th - 26th, 2017",
+      location: "Ann Arbor, Michigan",
+      description:
+        "Developed Super Graphic Air Traffic, a VR website made to introduce people to the world of air traffic controlling. This project was built completely using THREE.js as well as a node backend server.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/mhacks-9.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/threejs-planes",
+        },
+      ],
+    },
+    {
+      title: "StartHacks I",
+      dates: "March 4th - 5th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed at StartHacks 2017, Recipic is a mobile app which allows you to take pictures of ingredients around your house, and it will recognize those ingredients using ClarifAI image recognition API and return possible recipes to make. Recipic recieved 1st place at the hackathon for best pitch and hack.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/starthacks.png",
+      win: "1st Place Winner",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source (Mobile)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/recipic-ionic",
+        },
+        {
+          title: "Source (Server)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/recipic-rails",
+        },
+      ],
+    },
+    {
+      title: "QHacks II",
+      dates: "February 3rd - 5th, 2017",
+      location: "Kingston, Ontario",
+      description:
+        "Developed a mobile game which enables city-wide manhunt with random lobbies",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/qhacks.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source (Mobile)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/human-huntr-react-native",
+        },
+        {
+          title: "Source (API)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/human-huntr-rails",
+        },
+      ],
+    },
+    {
+      title: "Terrible Hacks V",
+      dates: "November 26th, 2016",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed a mock of Windows 11 with interesting notifications and functionality",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/terrible-hacks-v.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/justinmichaud/TerribleHacks2016-Windows11",
+        },
+      ],
+    },
+    {
+      title: "Portal Hackathon",
+      dates: "October 29, 2016",
+      location: "Kingston, Ontario",
+      description:
+        "Developed an internal widget for uploading assignments using Waterloo's portal app",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/portal-hackathon.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/UWPortalSDK/crowmark",
+        },
+      ],
+    },
+  ],
+} as const;
